cacfb92813d2ca91284a59493745bad79fce2797
Ruth Mares <hermosilloruth59@gmail.com>
2022-12-08 13:34:26 -0700

    Create Supers Model + Create Supers Serializer + Update Supers URLs + Add Views Function to Get Supers by ID

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SupersSerializer
+from .models import Supers
 
-# Create your views here.
+
+@api_view(['GET'])
+def supers_detail(request):
+    supers = get_object_or_404(Supers, pk=pk)
+    if request.method == 'GET':
+        serializer = SupersSerializer(supers);
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        
