1c5edf9d16fffc5dbd148d34773a50d48afe66b1
Ruth Mares <hermosilloruth59@gmail.com>
2022-12-10 13:07:04 -0700

    Update Project URLs files + Add POST Funcionality + Modify Supers Model to Fix Integrity Error

--- supers/urls.py
+++ supers/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.pass)
-    path('<int:pk>/', views.supers_detail)
+    path('', views.supers_list),
+    path('<int:pk>/', views.supers_detail),
 ]
--- supers/views.py
+++ supers/views.py
@@ -6,10 +6,32 @@ from .serializers import SupersSerializer
 from .models import Supers
 
 
+
+@api_view(['GET', 'POST'])
+def supers_list(request):
+    if request.method == 'GET':
+        super_type = request.query_params.get('super_type')
+        print(super_type)
+        
+        queryset = Supers.objects.all()
+
+        if super_type:
+            queryset = queryset.filter(super__type = super_type)
+        
+        serializer = SupersSerializer(queryset, many=True)
+        return Response(serializer.data, status= status.HTTP_200_OK)
+    
+    
+    elif request.method == 'POST':
+        serializer = SupersSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
 @api_view(['GET'])
-def supers_detail(request):
+def supers_detail(request, pk):
     supers = get_object_or_404(Supers, pk=pk)
     if request.method == 'GET':
         serializer = SupersSerializer(supers);
         return Response(serializer.data, status=status.HTTP_200_OK)
-        
